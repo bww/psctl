213b23fb0f0c136b17b5f29ba552a35f0f56f1d8
Brian W. Wolter <brianwolter@gmail.com>
2023-03-13 15:31:04 -0400

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
 use core::time;
+use std::process;
 
 use tokio;
+use clap::Parser;
 
 mod awaiter;
 
+use crate::awaiter::error;
+
+#[derive(Parser, Debug, Clone)]
+#[clap(author, version, about, long_about = None)]
+pub struct Options {
+  #[clap(long, help="Enable debugging mode")]
+  pub debug: bool,
+  #[clap(long, help="Enable verbose output")]
+  pub verbose: bool,
+  #[clap(help="Processes to manage")]
+  pub specs: Vec<String>,
+}
+
 #[tokio::main]
 async fn main() {
-  let u = vec![
-    "http://www.google.com",
-    "http://www.google.com",
-    "http://localhost/foobar",
-    "file:///Users/brian/Development/Products/psctl/Cargo.lock",
-  ];
-  match awaiter::wait(u, time::Duration::from_secs(10)).await {
-    Ok(())   => println!("Ok"),
-    Err(err) => panic!("Failed: {}", err),
-  }
+  match cmd().await {
+    Ok(_)     => {},
+    Err(err)  => {
+      eprintln!("* * * {}", err);
+      process::exit(1);
+    },
+  };
+}
+
+async fn cmd() -> Result<(), error::Error> {
+  let opts = Options::parse();
+  awaiter::wait(&opts.specs, time::Duration::from_secs(10)).await
 }
