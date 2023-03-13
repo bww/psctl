699819c8d1aa7a26fb8ef8a5c4b33be0da7262c1
Brian W. Wolter <brianwolter@gmail.com>
2023-03-13 14:31:29 -0400

    ...

--- src/awaiter/mod.rs
+++ src/awaiter/mod.rs
@@ -1,77 +1,80 @@
 pub mod error;
 
 use core::time;
+use std::path;
 use std::pin::Pin;
 use std::time::SystemTime;
 use std::result;
 
+use url;
 use futures::Future;
 use futures::future::try_join_all;
 use tokio::time::sleep;
 
-use url;
-
 type Result<T> = result::Result<T, error::Error>;
 
-pub struct Awaiter {
-  urls: Vec<String>,
+pub async fn check(urls: &Vec<String>, timeout: time::Duration) -> Result<()> {
+  let deadline = SystemTime::now() + timeout;
+  let mut jobs: Vec<Pin<Box<dyn futures::Future<Output = Result<()>>>>> = Vec::new();
+  for base in urls {
+    let url = url::Url::parse(base)?;
+    let scheme = url.scheme();
+    match scheme {
+      "http" | "https" => jobs.push(Box::pin(wait_http(base, deadline))),
+      "file"           => jobs.push(Box::pin(wait_file(base, deadline))),
+      _                => return Err(error::AwaitError::new(&format!("Scheme '{}' not supported: {}", scheme, base)).into())
+    }
+  }
+  try_join_all(jobs).await?;
+  Ok(())
 }
 
-impl Awaiter {
-  pub fn new(urls: Vec<String>) -> Awaiter {
-    Awaiter{
-      urls: urls,
+async fn wait_fn<F>(url: &str, deadline: SystemTime, func: F) -> Result<()> 
+where
+  F: Fn(String, time::Duration) -> Pin<Box<dyn Future<Output = Result<bool>>>>
+{
+  let wait = time::Duration::from_secs(1);
+  loop {
+    let before = SystemTime::now();
+    println!(">>> Polling: {}", url);
+    if match func(url.to_string(), deadline.duration_since(SystemTime::now())?).await {
+      Ok(res)  => res,
+      Err(err) => false,
+    } {
+      println!("... OK! {}", url);
+      return Ok(()); // success
     }
-  }
-  
-  pub async fn check(&self, timeout: time::Duration) -> Result<()> {
-    let deadline = SystemTime::now() + timeout;
-    let mut jobs: Vec<Pin<Box<dyn futures::Future<Output = Result<()>>>>> = Vec::new();
-    for base in &self.urls {
-      let url = url::Url::parse(base)?;
-      match url.scheme() {
-        "http" | "https" => jobs.push(Box::pin(self.wait_http(base, deadline))),
-        "file"           => jobs.push(Box::pin(self.wait_file(base, deadline))),
-        _                => return Err(error::AwaitError::new(&format!("Scheme '{}' not supported: {}", url.scheme(), base)).into())
+    let after = SystemTime::now();
+    if after + wait >= deadline {
+      return Err(error::AwaitError::new(&format!("Deadline exceeded: {}", url)).into());
+    } else {
+      let elapsed = after.duration_since(before)?;
+      if elapsed < wait {
+        println!("... Waiting: {:?}", wait - elapsed);
+        sleep(wait - elapsed).await;
       }
     }
-    try_join_all(jobs).await?;
-    Ok(())
   }
-  
-  async fn wait_http(&self, url: &str, deadline: SystemTime) -> Result<()> {
-    let wait = time::Duration::from_secs(1);
-    loop {
-      let before = SystemTime::now();
-      println!(">>> Polling: {}", url);
-      if match reqwest::Client::new().get(url).timeout(deadline.duration_since(SystemTime::now())?).send().await {
+}
+
+async fn wait_http(url: &str, deadline: SystemTime) -> Result<()> {
+  wait_fn(url, deadline, |u, t| {
+    Box::pin(async move {
+      Ok(match reqwest::Client::new().get(u).timeout(t).send().await {
         Ok(rsp)  => rsp.status().is_success(),
         Err(err) => false,
-      } {
-        println!("... OK! {}", url);
-        return Ok(()); // success
+      })
+    })
+  }).await
+}
+
+async fn wait_file(url: &str, deadline: SystemTime) -> Result<()> {
+  wait_fn(url, deadline, |u, t| {
+    Box::pin(async move {
+      match url::Url::parse(&u) {
+        Ok(u)    => Ok(path::Path::new(u.path()).exists()),
+        Err(err) => Err(err.into()),
       }
-      let after = SystemTime::now();
-      if after + wait >= deadline {
-        return Err(error::AwaitError::new(&format!("Deadline exceeded: {}", url)).into());
-      } else {
-        let elapsed = after.duration_since(before)?;
-        if elapsed < wait {
-          println!("... Waiting: {:?}", wait - elapsed);
-          sleep(wait - elapsed).await;
-        }
-      }
-    }
-  }
-  
-  async fn wait_file(&self, url: &str, deadline: SystemTime) -> Result<()> {
-    println!(">>> Starting: {}", url);
-    let rsp = reqwest::get(url).await?;
-    let status = rsp.status();
-    if status.is_success() {
-      Ok(())
-    }else{
-      Err(error::AwaitError::new(&format!("Invalid status code: {}", status)).into())
-    }
-  }
+    })
+  }).await
 }
--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ mod awaiter;
 
 #[tokio::main]
 async fn main() {
-  let a = awaiter::Awaiter::new(vec![
+  let u = vec![
     "http://www.google.com".to_owned(),
     "http://www.google.com".to_owned(),
     "http://localhost/foobar".to_owned(),
-    // "file:///Users/brian/Development/Products/psctl/Cargo.lock".to_owned(),
-  ]);
-  match a.check(time::Duration::from_secs(10)).await {
+    "file:///Users/brian/Development/Products/psctl/Cargo.lock".to_owned(),
+  ];
+  match awaiter::check(&u, time::Duration::from_secs(10)).await {
     Ok(())   => println!("Ok"),
     Err(err) => panic!("Failed: {}", err),
   }
