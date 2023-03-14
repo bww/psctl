093d7e7119b1407f4e727da292759e383ed301ae
Brian W. Wolter <brianwolter@gmail.com>
2023-03-13 20:42:02 -0400

    ...

--- src/runner/mod.rs
+++ src/runner/mod.rs
@@ -39,13 +39,20 @@ impl Pod {
 
 pub struct Process {
   command: String,
+  label: Option<String>,
+  deps: Vec<String>,
   check: Option<String>,
 }
 
 impl Process {
-  pub fn new(cmd: &str, url: Option<&str>) -> Process {
+  pub fn new(label: Option<&str>, cmd: &str, deps: Vec<&str>, url: Option<&str>) -> Process {
     Process{
       command: cmd.to_owned(),
+      label: match label {
+        Some(label) => Some(label.to_owned()),
+        None => None,
+      },
+      deps: deps.iter().map(|e| e.to_string()).collect(),
       check: match url {
         Some(url) => Some(url.to_owned()),
         None => None,
@@ -54,11 +61,32 @@ impl Process {
   }
   
   pub fn parse(text: &str) -> Result<Process> {
+    let split: Vec<&str> = text.splitn(2, ":").collect();
+    let (label, text) = match split.len() {
+      2 => (Some(split[0].trim()), split[1].trim()),
+      1 => (None, text),
+      _ => return Err(error::ExecError::new(&format!("Invalid process format: {}", text)).into()),
+    };
+    
     let split: Vec<&str> = text.splitn(2, "=").collect();
-    match split.len() {
-      2 => Ok(Self::new(split[0], Some(split[1]))),
-      1 => Ok(Self::new(split[0], None)),
-      _ => Err(error::ExecError::new(&format!("Invalid process format: {}", text)).into()),
+    let (cmd, check) = match split.len() {
+      2 => (split[0].trim(), Some(split[1].trim())),
+      1 => (split[0], None),
+      _ => return Err(error::ExecError::new(&format!("Invalid process format: {}", text)).into()),
+    };
+    
+    Ok(Self::new(label, cmd, Vec::new(), check))
+    // let (cmd, check) = match split.len() {
+    //   2 => Ok(Self::new(split[0], Some(split[1]))),
+    //   1 => Ok(Self::new(split[0], None)),
+    //   _ => Err(error::ExecError::new(&format!("Invalid process format: {}", text)).into()),
+    // }
+  }
+  
+  pub fn label<'a>(&'a self) -> Option<&'a str> {
+    match &self.label {
+      Some(label) => Some(label),
+      None => None,
     }
   }
   
@@ -90,18 +118,22 @@ impl Process {
 
 impl fmt::Display for Process {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    match self.check() {
-      Some(url) => write!(f, "{} ({})", self.command(), url),
-      None      => write!(f, "{}", self.command()),
+    let mut d = String::new();
+    match self.label() {
+      Some(l) => d.push_str(&format!("{}: ", l)),
+      None    => {},
     }
+    d.push_str(self.command());
+    match self.check() {
+      Some(u) => d.push_str(&format!(" ({})", u)),
+      None    => {},
+    }
+    write!(f, "{}", &d)
   }
 }
 
 impl fmt::Debug for Process {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    match self.check() {
-      Some(url) => write!(f, "{} ({})", self.command(), url),
-      None      => write!(f, "{}", self.command()),
-    }
+    fmt::Display::fmt(self, f)
   }
 }
