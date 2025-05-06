e11687d30c3d7c83c70c2ce32f8d793675dfd44c
Brian W. Wolter <brianwolter@gmail.com>
2025-05-06 14:35:09 -0400

    Adding MIT license

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,15 @@ mod runner;
 mod error;
 
 #[derive(Parser, Debug, Clone)]
-#[clap(author, version, about, long_about = None)]
+#[clap(
+  author, version,
+  about = "Process Control is an operator for interdependent processes.
+https://crates.io/crates/psctl",
+  long_about = None,
+  after_help = "EXAMPLES:
+
+    $ psctl 'a: echo A' 'b: echo B=file:///tmp/file' 'c +a,b: echo C'"
+)]
 pub struct Options {
   #[clap(long, help="Enable debugging mode")]
   pub debug: bool,
@@ -22,17 +30,22 @@ pub struct Options {
   pub verbose: bool,
   #[clap(long, help="Load process specifiers from a taskfile")]
   pub file: Option<String>,
-  #[clap(help="Process specifiers to manage. When a taskfile is provided, it is preferred:
+  #[clap(
+    help_heading="SPECIFIERS",
+    help="Task specifiers to run and manage. When a taskfile is provided, it is preferred over specifiers provided on the command line.
 
-specs   := <spec1> [... <specN>]
-spec    := <label> [<deps>]: <command>[=<check>]
-labels  := <label> | <label>, <labels>
-label   := /[a-zA-Z0-9]+/
-deps    := + <labels>
-command := /[^=]+/
-check   := any file:// or http(s):// url
+Any number of task specifiers may be provided as arguments. Each specifier has the following form:
 
-$ psctl 'a: echo A' 'b: echo B=file:///tmp/file' 'c +a,b: echo C'
+    spec     := <label> [<deps>]: <command>[=<check>]
+    labels   := <label1> [... <labelN>]
+    label    := /[a-zA-Z0-9]+/
+    deps     := '+' <labels>
+    command  := /[^=]+/
+    check    := a file:// or http(s):// url
+
+EXAMPLE
+
+    $ psctl 'a: echo A' 'b: echo B=file:///tmp/file' 'c +a,b: echo C'
 ")]
   pub specs: Vec<String>,
 }
